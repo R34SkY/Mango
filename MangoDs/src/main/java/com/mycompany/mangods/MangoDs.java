b72a0ad42fac28b23b2ca0b468d3a62ca7f082b4
R34SkY <2311CS010406@mallareddyuniversity.ac.in>
2024-08-16 20:16:41 +0530

    Various Sorts

---
+++ MangoDs/src/main/java/com/mycompany/mangods/MangoDs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mangods;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MangoDs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
